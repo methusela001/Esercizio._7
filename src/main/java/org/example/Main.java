c693e37cb7e906ded1c45cc98756f869851e9a5a
Methusela Fernando <136374272+methusela001@users.noreply.github.com>
2023-07-20 23:04:59 +0200

    Update Main.java
    new

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         // Define a variable called myName and assign your name to it.
-        String myName = ("Methusela")
+        String myName = ("Methusela");
         //Using the if-else statement, print if the number of letters in your name is even or odd.
         int lengthOfName = myName.length();
         if (lengthOfName % 2 !=0 ){
